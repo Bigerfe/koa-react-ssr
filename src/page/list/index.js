f522b6a76b0769bdafdc14143e05884e51cd5859
zhubaba <zhangjiapeng@xin.com>
2019-08-16 22:32:53 +0800

    下一步处理 fetch 同构

--- src/page/list/index.js
+++ src/page/list/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import RootContext from '../../app/route-context';
 import {
     Link
 } from 'react-router-dom';
 export default class Index extends React.Component {
-    constructor(props) {
+    constructor(props,context) {
         super(props);
     }
+    //得到 context 对象
+    static contextType = RootContext;
 
     static getInitialProps() {
         return [{
-            id: 100,
-            name: 'this is detail page'
+            id: 200,
+            name: '哈哈我去，我终于胖了。。。。'
         }]
     }
     render() {
-        return <div>当前是 列表页面 <Link to="/index">返回首页</Link></div>
+        return <React.Fragment>
+        
+        <Link to="/index">返回首页</Link>
+        
+        {
+            this.context.initialData.map(item=>(<div key={item.id}>{item.name}</div>))
+        }
+
+        </React.Fragment > 
     }
 }
