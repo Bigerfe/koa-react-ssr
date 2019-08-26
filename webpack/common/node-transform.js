d4bef24e4ccec89e2a79dc1be62a01da16eb87c7
zhubaba <zhangjiapeng@xin.com>
2019-08-27 01:37:23 +0800

    开发体验优化，目录同步创建，文件保存后即可同步，404设置 z 开头，为了放入路由末尾

--- webpack/common/node-transform.js
+++ webpack/common/node-transform.js
@@ -23,17 +23,11 @@ function throttle(filepath, callback) {
 function compileWatcher(filepath) {
     //dev环境下 babel用的是node环境需要区分
     process.env.BABEL_ENV = 'node';
-    //process.env.NODE_ENV = 'test';
     console.log('file watcher', filepath);
     var fileName = /(src|server).*/.exec(filepath)[0];
     var ext = path.extname(fileName);
 
-    console.log('-----get change file name -----', fileName);
-
     var newpath = path.resolve('dist/server', fileName);
-
-    console.log(newpath);
-
 
     if (/\.(js|jsx)$/.test(ext)) {
         //监听 node server 文件
@@ -47,10 +41,10 @@ function compileWatcher(filepath) {
 
         //, { stdio: 'inherit' } 此处不必重复输出
         spawnSync.sync('babel', [filepath, '--out-file', newpath]);
-        console.log(chalk.yellow('compiled ' + filepath + ' to ' + newpath));
+        console.log(chalk.yellow('compiled0 ' + filepath + ' to ' + newpath));
 
     } //TODO:这里代码有点混乱，后期改进  合并 react 路由入口
-    else if (/\.(js|jsx)$/.test(filepath) && filepath.indexOf('/src/page/') > 0 && filepath.indexOf('/config/route.js') > -1) {
+    if (/\.(js|jsx)$/.test(filepath) && filepath.indexOf('/src/page/') > 0 && filepath.indexOf('/config/route.js') > -1) {
         //监听客户端路由文件  且不是路由入口文件
         //, { stdio: 'inherit' } 此处不必重复输出
         spawnSync.sync('npm', ['run', 'chai-routes'], {
@@ -60,7 +54,7 @@ function compileWatcher(filepath) {
         console.log(chalk.yellow('chai-routes compiled ' + filepath));
     }
     //
-    else if (/\.(ejs|tpl|html)$/.test(ext)) {
+    if (/\.(ejs|tpl|html)$/.test(ext)) {
         try {
             //非js文件如template文件只负责到指定文件夹即可
             fs.copyFileSync(filepath, newpath);
@@ -70,7 +64,8 @@ function compileWatcher(filepath) {
             console.log(error);
             process.exit(1);
         }
-    } else if (/\.(scss|less|css)$/.test(ext)) {
+    }
+    if (/\.(scss|less|css)$/.test(ext)) {
         console.log(chalk.yellow('edited ' + filepath));
     }
 
