9a15d96eb2f3eac35aab2b331eb8ab1d393e78cd
zhubaba <zhangjiapeng@xin.com>
2019-08-27 17:14:40 +0800

    增加 html 文件的构建同步

---
+++ webpack/scripts/copy-not-js.js
@@ -0,0 +1,26 @@
+/*******
+ * 
+ * 拷贝非 js 文件
+ * 在发布到生产环境的时候会执行此文件
+ */
+const fs = require('fs');
+const path  = require('path');
+
+
+const FIELS=[
+    {
+        src:'../../server/krs-base/temp/ssr.html',
+        dest: '../../dist/server/server/krs-base/temp/ssr.html'
+    }
+];
+
+function copyFile() {
+    FIELS.forEach(item=>{
+        fs.copyFileSync(path.resolve(__dirname,item.src),path.resolve(__dirname,item.dest));
+    })
+}
+
+copyFile();
+console.log('htmls copy success');
+console.log('please wait,webpack start building.....');
+
