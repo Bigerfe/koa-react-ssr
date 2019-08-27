763f58b662de8b1b28ad379da07f589019e5c050
zhubaba <zhangjiapeng@xin.com>
2019-08-27 19:11:55 +0800

    删除服务端 html 的控制台打印

--- server/krs-base/common/ssr/index.js
+++ server/krs-base/common/ssr/index.js
@@ -90,7 +90,6 @@ export default async (ctx) => {
     if(config.isSSR){
         console.log('render html =======================');
         const res = await getComponentHtml(ctx);
-        console.log(res);
         renderData.htmlContent = res.html;
         renderData.propsData = `<textarea style="display:none" id="krs-server-render-data-BOX">${JSON.stringify({ initialData: res.initialData||null })}</textarea>` ;
         renderData.config = config.cdnHost;
